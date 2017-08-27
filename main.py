dbc03e4de8dd9afded28d08ab4a9fa212c8a3721
blgo <blas_1990_@hotmail.com>
2017-08-27 11:41:51 +0100

    Add fallback sndmap Map when no locations are found in twitts

--- main.py
+++ main.py
@@ -35,17 +35,26 @@ def index():
     except Exception as error:
         return render_template("error.html", error=str(error))
     # creating a map in the view
-    try:
+    if len(gmmarkers.markers) > 0:
+        try:
+            sndmap = Map(
+                identifier="sndmap",
+                lat=gmmarkers.markers[0].get('lat'),
+                lng=gmmarkers.markers[0].get('lng'),
+                style="height:300px;width:90%;margin:0;",
+                zoom=3,
+                markers=gmmarkers.markers
+            )
+        except Exception as error:
+            return render_template("error.html", error=str(error))
+    else:
         sndmap = Map(
-            identifier="sndmap",
-            lat=gmmarkers.markers[0].get('lat'),
-            lng=gmmarkers.markers[0].get('lng'),
-            style="height:300px;width:90%;margin:0;",
-            zoom=3,
-            markers=gmmarkers.markers
+                identifier="sndmap",
+                lat=0,
+                lng=0,
+                style="height:300px;width:90%;margin:0;",
+                zoom=3,
         )
-    except Exception as error:
-        return render_template("error.html", error=str(error))
     return render_template("index.html", twitts=twitts, sndmap=sndmap)
 
 
